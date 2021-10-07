e234cefa86eadc2839f183514625131d022adec3
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 14:23:23 -0300

    exercise 2-f completed

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -20,3 +20,9 @@ var newFood = myFood.substring(0,1).toUpperCase()+myFood.substring(2,18).toLower
 // 2-e) - Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
 var myBrothers = "i have two brothers";
 var blankSpace = myBrothers.indexOf(" ");
+
+// 2-f) - Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
+var longWords = "multidimensional development";
+console.log(longWords.indexOf(" "));
+var newWords = longWords.substring(0,1).toUpperCase()+longWords.substring(1,16).toLowerCase()+longWords.substring(17,18).toUpperCase()+longWords.substring(18,28).toLowerCase();
+console.log(newWords)
