e18e81e6c851d0f0d9d0393f2e52b774eb0b599a
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 13:23:02 -0300

    exercise 2-e completed

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -16,3 +16,7 @@ var lastThree = clubFan.substring(24,27);
 var myFood = "i like hamburguers";
 console.log(myFood.length);
 var newFood = myFood.substring(0,1).toUpperCase()+myFood.substring(2,18).toLowerCase();
+
+// 2-e) - Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+var myBrothers = "i have two brothers";
+var blankSpace = myBrothers.indexOf(" ");
