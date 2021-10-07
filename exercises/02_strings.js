73961d85a73eff484172a296b086253345176a05
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 13:04:13 -0300

    exercise 2-c completed

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -6,3 +6,8 @@ console.log(thingsIlike.toUpperCase());
 var fruits = "Apple, Banana, Tangerine"
 var firstFruit = fruits.substring(0,5);
 console.log(firstFruit);
+
+// 2-c) - Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
+var clubFan = "I am a Newells Old Boys fan"
+console.log(clubFan.length)
+var lastThree = clubFan.substring(24,27);
