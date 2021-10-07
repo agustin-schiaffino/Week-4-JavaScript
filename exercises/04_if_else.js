30f7368fcf6595d8b4c851726641539b3badb67c
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 17:15:11 -0300

    uncomment exercise 4-a

--- exercises/04_if_else.js
+++ exercises/04_if_else.js
@@ -1,10 +1,10 @@
-/* 4-a) - Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
+// 4-a) - Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
 var numb = Math.random();
 if (numb >= 0.5) {
     alert ("Greater than 0.5")
 }   else {
     alert ("Lower than 0.5")
-}*/
+}
 
 /* 4-b) - Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
 “Bebe” si la edad es menor a 2 años;
