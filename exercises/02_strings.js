69bc2b48e6f93b35c94862e8aab7f3e5d9371a9c
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 12:30:37 -0300

    exercise 2-b completed

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -1,3 +1,8 @@
 // 2-a) - Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
-var thingsIlike = "I like playing videogames"
-console.log(thingsIlike.toUpperCase())
+var thingsIlike = "I like playing videogames";
+console.log(thingsIlike.toUpperCase());
+
+// 2-b) - Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+var fruits = "Apple, Banana, Tangerine"
+var firstFruit = fruits.substring(0,5);
+console.log(firstFruit);
