f00d8e88a8a550a20b787566cd4c5b4756205850
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 14:52:52 -0300

    add spaces to lines 18 and 27

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -15,7 +15,7 @@ var lastThree = clubFan.substring(24,27);
 // 2-d) - Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
 var myFood = "i like hamburguers";
 console.log(myFood.length);
-var newFood = myFood.substring(0,1).toUpperCase()+myFood.substring(2,18).toLowerCase();
+var newFood = myFood.substring(0,1).toUpperCase()+" "+myFood.substring(2,18).toLowerCase();
 
 // 2-e) - Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
 var myBrothers = "i have two brothers";
@@ -24,5 +24,5 @@ var blankSpace = myBrothers.indexOf(" ");
 // 2-f) - Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
 var longWords = "multidimensional development";
 console.log(longWords.indexOf(" "));
-var newWords = longWords.substring(0,1).toUpperCase()+longWords.substring(1,16).toLowerCase()+longWords.substring(17,18).toUpperCase()+longWords.substring(18,28).toLowerCase();
+var newWords = longWords.substring(0,1).toUpperCase()+longWords.substring(1,16).toLowerCase()+" "+longWords.substring(17,18).toUpperCase()+longWords.substring(18,28).toLowerCase();
 console.log(newWords)
