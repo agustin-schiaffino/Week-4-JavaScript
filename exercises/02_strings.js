688c203fb9826feefe274d64a8dde4d3a643a003
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 12:21:28 -0300

    exercise 2-a completed

---
+++ exercises/02_strings.js
@@ -0,0 +1,3 @@
+// 2-a) - Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+var thingsIlike = "I like playing videogames"
+console.log(thingsIlike.toUpperCase())
