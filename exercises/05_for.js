5f4f019a3fa3c0e86a1f3117e98ae190445c92a2
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-07 17:37:05 -0300

    exercise 5-a completed

---
+++ exercises/05_for.js
@@ -0,0 +1,5 @@
+// 5-a) - Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+var fiveWords = ["Hamburguer", "Pizza", "Pasta", "Salad", "Bacon"];
+for (let i = 0; i < fiveWords.length; i++) {
+    alert(fiveWords[i]);
+}
