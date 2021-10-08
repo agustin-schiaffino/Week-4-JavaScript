44007212fede1e20eff6f5d454d02a88f82668f5
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-08 11:24:46 -0300

    exercise 6-a completed

---
+++ exercises/06_functions.js
@@ -0,0 +1,10 @@
+// 6-a) - Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+
+function myFunction(a,b) {
+    var outCome = a+b;
+    console.log(outCome);
+}
+
+myFunction(10,20);
+
+
