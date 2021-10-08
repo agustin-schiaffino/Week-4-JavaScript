14f398a02132a4de55ea6e4d9ae10e0bd47ab4da
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-08 15:34:06 -0300

    function number corrected

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -21,8 +21,9 @@ secondFunc(5,5);
 
 // 6-c) - Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
 
-function validateInteger(x) {
+function numberCheck(x) {
     console.log(Number.isInteger(x));
 }
 
-validateInteger(50);
+numberCheck(50);
+
