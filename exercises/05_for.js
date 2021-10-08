27a17dde0ad24961b89ec5a38246d17d2368c42d
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-08 10:28:40 -0300

    exercise 5-c completed

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -4,9 +4,17 @@ for (let i = 0; i < fiveWords.length; i++) {
     alert(fiveWords[i]);
 }
 
-// 5-b) - Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
+/// 5-b) - Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
 
 for (let i = 0; i < fiveWords.length; i++) {
     alert(fiveWords[i].substring(0,1).toUpperCase()+(fiveWords[i].substring(1).toLowerCase()));
 }
 
+//5-c) - Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
+var sentence = "";
+var fiveWords = ["hamburguer", "pizza", "pasta", "salad", "bacon"];
+
+for (let i = 0; i < fiveWords.length; i++) {
+    sentence+=fiveWords[i]+", ";
+}
+alert(sentence)
