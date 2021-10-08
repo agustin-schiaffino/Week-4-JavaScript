4c411a14e977549ac8eca26114e0b05385070239
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-10-08 10:49:03 -0300

    exercise 5-d completed

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -10,7 +10,7 @@ for (let i = 0; i < fiveWords.length; i++) {
     alert(fiveWords[i].substring(0,1).toUpperCase()+(fiveWords[i].substring(1).toLowerCase()));
 }
 
-//5-c) - Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
+// 5-c) - Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
 var sentence = "";
 var fiveWords = ["hamburguer", "pizza", "pasta", "salad", "bacon"];
 
@@ -18,3 +18,11 @@ for (let i = 0; i < fiveWords.length; i++) {
     sentence+=fiveWords[i]+", ";
 }
 alert(sentence)
+
+// 5-d) - Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).
+var empty = [];
+
+for (let i = 0; i < 10 ; i++) {
+    empty[i] = i;
+}
+console.log(empty);
